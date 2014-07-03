175b205159dd01c1434a42d155ed1fe752c79eab
Alejandro Lopez Osornio <alopez@termmed.com>
2014-07-03 16:52:39 -0300

    debugging full text search 5

--- routes/snomed.js
+++ routes/snomed.js
@@ -30,6 +30,7 @@ router.get('/:db/:collection/concepts/:sctid?', function(req, res) {
                 console.log(err.message);
             }
             if (doc) {
+                res.status(200);
                 res.header('Content-Type', 'application/json');
                 res.send(doc);
             } else {
@@ -70,6 +71,7 @@ router.get('/:db/:collection/concepts/:sctid/descriptions/:descriptionId?', func
                             result.push(desc);
                         }
                     });
+                    res.status(200);
                     res.send(result);
                 } else {
                     res.status(404);
@@ -113,6 +115,7 @@ router.get('/:db/:collection/concepts/:sctid/relationships?', function(req, res)
                             result.push(desc);
                         });
                     }
+                    res.status(200);
                     res.send(result);
                 } else {
                     res.status(404);
@@ -154,6 +157,7 @@ router.get('/:db/:collection/concepts/:sctid/children?', function(req, res) {
                     docs.forEach(function(doc) {
                         result.push(doc);
                     });
+                    res.status(200);
                     res.send(result);
                 } else {
                     res.status(404);
@@ -205,6 +209,7 @@ router.get('/:db/:collection/concepts/:sctid/parents?', function(req, res) {
                             });
                         }
                     }
+                    res.status(200);
                     res.send(result);
                 } else {
                     res.status(404);
@@ -367,6 +372,7 @@ router.get('/:db/:collection/descriptions/:sctid?', function(req, res) {
                             var duration = Date.now() - start;
                             logger.log('info', 'Search for ' + searchTerm + ' result = ' + docs.length, {searchTerm: searchTerm, database: req.params.db, collection: req.params.collection, searchMode: searchMode, language: lang, statusFilter: statusFilter, matches: docs.length, duration: duration, dbduration: dbDuration});
                             res.header('Content-Type', 'application/json');
+                            res.status(200);
                             res.send(result);
                         }
                     } else {
@@ -387,74 +393,74 @@ router.get('/:db/:collection/descriptions/:sctid?', function(req, res) {
             console.log(query);
             options.score = { $meta: "textScore" };
             collection.find(query, options, function (err, cursor) {
-                console.log("Error? : " + JSON.stringify(err));
+                console.log("Error? : " + err);
                 cursor.toArray(function(err, docs) {
-                    console.log(JSON.stringify(docs));
-                });
-
-                var dbDuration = Date.now() - start;
-                if (err) {
-                    console.log(e, 'error');
-                    var duration = Date.now() - start;
-                    //logger.log('error', 'Search for ' + searchTerm + ' ERROR', {searchTerm: searchTerm, database: req.params.db, collection: req.params.collection, searchMode: searchMode, language: lang, statusFilter: statusFilter, duration: duration, dbDuration: dbDuration});
-                    res.send(501);
-                } else {
-                    var result = {};
-                    result.matches = [];
-                    result.details = {'total': cb.results.length, 'skipTo': skipTo, 'returnLimit': returnLimit};
-                    result.filters = {};
-                    result.filters.lang = {};
-                    result.filters.semTag = {};
-                    var matchedDescriptions = cb.results.slice(0);
-                    matchedDescriptions.sort(function (a, b) {
-                        if (a.score > b.score)
-                            return -1;
-                        if (a.score < b.score)
-                            return 1;
-                        if (a.score == b.score) {
-                            if (a.obj.term.length < b.obj.term.length)
-                                return -1;
-                            if (a.obj.term.length > b.obj.term.length)
-                                return 1;
-                        }
-                        return 0;
-                    });
-                    if (matchedDescriptions.length > 0) {
-                        var count = 0;
-                        matchedDescriptions.forEach(function (doc) {
-                            if (semanticFilter == "none" || (semanticFilter == doc.obj.semanticTag)) {
-                                if (langFilter == "none" || (langFilter == doc.obj.lang)) {
-                                    if (count >= skipTo && count < (skipTo + returnLimit)) {
-                                        result.matches.push({"term": doc.obj.term, "conceptId": doc.obj.conceptId, "active": doc.obj.active, "conceptActive": doc.obj.conceptActive, "fsn": doc.obj.fsn});
-                                    }
-                                    if (result.filters.semTag.hasOwnProperty(doc.obj.semanticTag)) {
-                                        result.filters.semTag[doc.obj.semanticTag] = result.filters.semTag[doc.obj.semanticTag] + 1;
-                                    } else {
-                                        result.filters.semTag[doc.obj.semanticTag] = 1;
-                                    }
-                                    if (result.filters.lang.hasOwnProperty(doc.obj.lang)) {
-                                        result.filters.lang[doc.obj.lang] = result.filters.lang[doc.obj.lang] + 1;
-                                    } else {
-                                        result.filters.lang[doc.obj.lang] = 1;
-                                    }
-                                    count = count + 1;
-                                }
-                            }
-                        });
-                        result.details.total = count;
+                    console.log("Results size: " + docs.length);
+                    var dbDuration = Date.now() - start;
+                    if (err) {
+                        console.log(e, 'error');
                         var duration = Date.now() - start;
-                        //logger.log('info', 'Search for ' + searchTerm + ' result = ' + cb.results.length, {searchTerm: searchTerm, database: req.params.db, collection: req.params.collection, searchMode: searchMode, language: lang, statusFilter: statusFilter, matches: cb.results.length, duration: duration, dbDuration: dbDuration});
-                        res.header('Content-Type', 'application/json');
-                        res.send(result);
+                        //logger.log('error', 'Search for ' + searchTerm + ' ERROR', {searchTerm: searchTerm, database: req.params.db, collection: req.params.collection, searchMode: searchMode, language: lang, statusFilter: statusFilter, duration: duration, dbDuration: dbDuration});
+                        res.send(501);
                     } else {
-                        var duration = Date.now() - start;
-                        //logger.log('info', 'Search for ' + searchTerm + ' result = ' + cb.results.length, {searchTerm: searchTerm, database: req.params.db, collection: req.params.collection, searchMode: searchMode, language: lang, statusFilter: statusFilter, matches: cb.results.length, duration: duration, dbDuration: dbDuration});
                         var result = {};
                         result.matches = [];
-                        result.details = {'total': 0, 'skipTo': skipTo, 'returnLimit': returnLimit};
-                        res.send(result);
+                        result.details = {'total': docs.length, 'skipTo': skipTo, 'returnLimit': returnLimit};
+                        result.filters = {};
+                        result.filters.lang = {};
+                        result.filters.semTag = {};
+                        var matchedDescriptions = docs.slice(0);
+                        matchedDescriptions.sort(function (a, b) {
+                            if (a.score > b.score)
+                                return -1;
+                            if (a.score < b.score)
+                                return 1;
+                            if (a.score == b.score) {
+                                if (a.obj.term.length < b.obj.term.length)
+                                    return -1;
+                                if (a.obj.term.length > b.obj.term.length)
+                                    return 1;
+                            }
+                            return 0;
+                        });
+                        if (matchedDescriptions.length > 0) {
+                            var count = 0;
+                            matchedDescriptions.forEach(function (doc) {
+                                if (semanticFilter == "none" || (semanticFilter == doc.obj.semanticTag)) {
+                                    if (langFilter == "none" || (langFilter == doc.obj.lang)) {
+                                        if (count >= skipTo && count < (skipTo + returnLimit)) {
+                                            result.matches.push({"term": doc.obj.term, "conceptId": doc.obj.conceptId, "active": doc.obj.active, "conceptActive": doc.obj.conceptActive, "fsn": doc.obj.fsn});
+                                        }
+                                        if (result.filters.semTag.hasOwnProperty(doc.obj.semanticTag)) {
+                                            result.filters.semTag[doc.obj.semanticTag] = result.filters.semTag[doc.obj.semanticTag] + 1;
+                                        } else {
+                                            result.filters.semTag[doc.obj.semanticTag] = 1;
+                                        }
+                                        if (result.filters.lang.hasOwnProperty(doc.obj.lang)) {
+                                            result.filters.lang[doc.obj.lang] = result.filters.lang[doc.obj.lang] + 1;
+                                        } else {
+                                            result.filters.lang[doc.obj.lang] = 1;
+                                        }
+                                        count = count + 1;
+                                    }
+                                }
+                            });
+                            result.details.total = count;
+                            var duration = Date.now() - start;
+                            //logger.log('info', 'Search for ' + searchTerm + ' result = ' + cb.results.length, {searchTerm: searchTerm, database: req.params.db, collection: req.params.collection, searchMode: searchMode, language: lang, statusFilter: statusFilter, matches: cb.results.length, duration: duration, dbDuration: dbDuration});
+                            res.header('Content-Type', 'application/json');
+                            res.send(result);
+                        } else {
+                            var duration = Date.now() - start;
+                            //logger.log('info', 'Search for ' + searchTerm + ' result = ' + cb.results.length, {searchTerm: searchTerm, database: req.params.db, collection: req.params.collection, searchMode: searchMode, language: lang, statusFilter: statusFilter, matches: cb.results.length, duration: duration, dbDuration: dbDuration});
+                            var result = {};
+                            result.matches = [];
+                            result.details = {'total': 0, 'skipTo': skipTo, 'returnLimit': returnLimit};
+                            res.status(200);
+                            res.send(result);
+                        }
                     }
-                }
+                });
                 db.close();
             });
         });
