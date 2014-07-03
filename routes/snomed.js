79a152883350ff79f894879e1887ba2b7fc57ffe
Alejandro Lopez Osornio <alopez@termmed.com>
2014-07-03 16:56:46 -0300

    debugging full text search 7

--- routes/snomed.js
+++ routes/snomed.js
@@ -390,9 +390,10 @@ router.get('/:db/:collection/descriptions/:sctid?', function(req, res) {
     } else if (searchMode == "fullText") {
         MongoClient.connect("mongodb://localhost:27017/" + req.params.db, function (err, db) {
             var collection = db.collection(req.params.collection + 'tx');
+            console.log(req.params.collection + 'tx');
             console.log(query);
-            options.score = { $meta: "textScore" };
-            collection.find(query, function (err, cursor) {
+            //options.score = { $meta: "textScore" };
+            collection.find(query, options, function (err, cursor) {
                 console.log("Error? : " + err);
                 cursor.toArray(function(err, docs) {
                     console.log("Results size: " + docs.length);
