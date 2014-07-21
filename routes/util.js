b6fbe2563b2c632632e46467ae311c4420a9b1e2
Alejandro Lopez Osornio <alopez@termmed.com>
2014-07-21 20:04:39 -0300

    Adding SVG to PNG UUDI as file name

--- routes/util.js
+++ routes/util.js
@@ -14,17 +14,18 @@ var appDir = path.dirname(topModule.filename);
 router.post('/svg2png', function(req, res) {
     var pngLink = "";
     var svg = req.body;
-    fs.writeFile(appDir + "/public/svg2pngTemp/1.svg", svg.svgContent, function(err) {
+    var id = guid();
+    fs.writeFile(appDir + "/public/svg2pngTemp/" + id + ".svg", svg.svgContent, function(err) {
         if(err) {
             console.log(err);
         } else {
-            svg2png(appDir + "/public/svg2pngTemp/1.svg", appDir +
-                "/public/svg2pngTemp/1.png", 3, function (err) {
+            svg2png(appDir + "/public/svg2pngTemp/" + id + ".svg", appDir +
+                "/public/svg2pngTemp/" + id + ".png", 3, function (err) {
                 if (err) {
                     console.log(err);
                     res.end("Error");
                 } else {
-                    pngLink = "svg2pngTemp/1.png";
+                    pngLink = "svg2pngTemp/" + id + ".png";
                     res.end(pngLink);
                 }
             });
@@ -35,14 +36,27 @@ router.post('/svg2png', function(req, res) {
 
 router.post('/saveSvg', function(req, res) {
     var pngLink = "";
+    var id = guid();
     var svg = req.body;
-    fs.writeFile(appDir + "/public/svg2pngTemp/2.svg", svg.svgContent, function(err) {
+    fs.writeFile(appDir + "/public/svg2pngTemp/" + id + ".svg", svg.svgContent, function(err) {
         if(err) {
         } else {
-            pngLink = "svg2pngTemp/2.svg";
+            pngLink = "svg2pngTemp/" + id + ".svg";
             res.end(pngLink);
         }
     });
 });
+
+var guid = function() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+    return function() {
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+            s4() + '-' + s4() + s4() + s4();
+    };
+};
 
 module.exports = router;
