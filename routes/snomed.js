e50447d568e6d5f470e513b91bceb5f95fb76894
Alejandro Lopez Osornio <alopez@termmed.com>
2014-07-30 16:01:07 -0300

    Adding count in nested query for members

--- routes/snomed.js
+++ routes/snomed.js
@@ -271,22 +271,24 @@ router.get('/:db/:collection/concepts/:sctid/members?', function(req, res) {
             return;
         }
         var collection = db.collection(req.params.collection);
-        collection.find(query, options).sort({defaultTerm: 1}, function(err, cursor) {
-            cursor.toArray(function(err, docs) {
-                var result = {};
-                result.members = [];
-                result.details = {'total': docs.length, 'refsetId': idParam };
-                if (docs.length > 0) {
-                    docs.forEach(function(doc) {
-                        result.members.push(doc);
-                    });
-                    res.status(200);
-                    res.send(result);
-                } else {
-                    res.status(200);
-                    res.send(result);
-                }
-                db.close();
+        collection.find(query, options).count(function (err, total) {
+            collection.find(query, options).sort({defaultTerm: 1}, function (err, cursor) {
+                cursor.toArray(function (err, docs) {
+                    var result = {};
+                    result.members = [];
+                    result.details = {'total': total, 'refsetId': idParam };
+                    if (docs.length > 0) {
+                        docs.forEach(function (doc) {
+                            result.members.push(doc);
+                        });
+                        res.status(200);
+                        res.send(result);
+                    } else {
+                        res.status(200);
+                        res.send(result);
+                    }
+                    db.close();
+                });
             });
         });
     });
