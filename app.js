f1e362e70548674eb86840471dc01f061e7e1044
alopezo <8TPFdVPnehjv4d>
2017-03-10 20:11:31 -0300

    Correcting diacritics conversion

--- app.js
+++ app.js
@@ -114,12 +114,41 @@ app.use(function(err, req, res, next) {
 
 });
 
+var cluster = require('cluster');
 var port = process.env.PORT || 3000;
-var server = require('http').Server(app);
 
-server.listen(port);
+if(cluster.isMaster) {
+    var numWorkers = require('os').cpus().length;
 
-console.log('Express app started on port '+port);
+    console.log('Master cluster setting up ' + numWorkers + ' workers...');
+
+    for(var i = 0; i < numWorkers; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('online', function(worker) {
+        console.log('Worker ' + worker.process.pid + ' is online');
+    });
+
+    cluster.on('exit', function(worker, code, signal) {
+        console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
+        console.log('Starting a new worker');
+        cluster.fork();
+    });
+} else {
+    //var app = require('express')();
+    // app.all('/*', function(req, res) {res.send('process ' + process.pid + ' says hello!').end();})
+
+    var server = app.listen(port, function() {
+        console.log('Process ' + process.pid + ' is listening in port ' + port + ' to all incoming requests');
+    });
+}
+
+// var server = require('http').Server(app);
+//
+// server.listen(port);
+//
+// console.log('Express app started on port '+port);
 
 module.exports = app;
 module.exports.accessControlConfig = accessControlConfig;
